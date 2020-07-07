925aac48fc2cef3ecdcacead2c1c238952bdbd14
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2020-07-07 13:23:28 -0500

    Utilities for min distance

--- land_utils.py
+++ land_utils.py
@@ -1,5 +1,8 @@
 import geopandas as gpd 
 import pandas as pd 
+from typing import List
+from shapely.ops import unary_union
+from shapely.geometry import Polygon
 
 def add_block_id_to_buildings(bldgs: gpd.GeoDataFrame,
                               blocks: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -40,4 +43,40 @@ def building_density_per_block(bldgs: gpd.GeoDataFrame,
 
     return blocks 
 
- 
+
+def max_min_distance(block_geom: Polygon, 
+                     building_list: List[Polygon],
+                     buffer_amt = 0.01) -> float:
+    '''
+
+    '''
+
+    # Add the block's exterior to the building geom list
+    block_bound_poly = block_geom.envelope.buffer(buffer_amt)
+    block_exterior = block_bound_poly.difference(block_geom)
+    building_list.append(block_exterior)
+
+    # Make the buildings one geom
+    other_geoms = unary_union(building_list)
+
+    # Calculate hausdorff distance
+    dist = block_geom.hausdorff_distance(other_geoms)
+
+    return dist 
+
+def get_block_building_list(bldgs: gpd.GeoDataFrame,
+                            blocks: gpd.GeoDataFrame,
+                            block_id: str) -> Tuple[Polygon, List[Polygon]]:
+    '''
+    Just a helper to get the buildings and block polygons 
+    from their repsective sources, from the desired block_id
+    '''
+
+    block_geom = blocks[blocks['block_id']==block_id]['geometry'].iloc[0]
+    building_list = list(bldgs[bldgs['block_id']==block_id]['geometry'].values)
+
+    return block_geom, building_list
+
+
+
+

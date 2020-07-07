cf68e6820df66e012441e470cee5f1c07e8d12a0
Cooper Nederhood <cnederhood@cs.uchicago.edu>
2020-07-07 14:44:39 -0500

    Utils for dist

--- land_utils.py
+++ land_utils.py
@@ -34,12 +34,18 @@ def building_density_per_block(bldgs: gpd.GeoDataFrame,
     assert 'block_id' in bldgs.columns, "ERROR: bldgs dataframe does not have block_id"
 
     bldgs['bldg_area'] = bldgs.area 
-    bldg_area_by_block = bldgs[['block_id', 'bldg_area']].groupby('block_id').sum()
+    bldgs['bldg_count'] = 1
+    bldg_area_by_block = bldgs[['block_id', 'bldg_area', 'bldg_count']].groupby('block_id').sum()
     bldg_area_by_block.reset_index(inplace=True)
 
+    for c in ['bldg_area', 'bldg_count']:
+        if c in blocks.columns:
+            blocks.drop(columns=[c], inplace=True)
     blocks = blocks.merge(bldg_area_by_block, how='left', on='block_id')
     blocks['block_area'] = blocks.area 
     blocks['bldg_density'] = blocks['bldg_area'] / blocks['block_area']
+
+    blocks.fillna(value=0.0, inplace=True)
 
     return blocks 
 
@@ -78,5 +84,24 @@ def get_block_building_list(bldgs: gpd.GeoDataFrame,
     return block_geom, building_list
 
 
+def max_building_distance_per_block(bldgs: gpd.GeoDataFrame,
+                                    blocks: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    '''
+    Adds a column to blocks dataframe which contains max
+    building distance 
+    '''
 
+    assert 'block_id' in bldgs.columns, "ERROR: bldgs dataframe does not have block_id"
+
+    new_data = []
+    all_block_ids = blocks['block_id'].values
+
+    for block_id in all_block_ids:
+        block_geom, building_list = get_block_building_list(bldgs, blocks, block_id)
+
+        max_dist = max_min_distance(block_geom, building_list)
+        new_data.append(max_dist)
+
+    blocks['max_dist'] = new_data
+    return blocks 
 
